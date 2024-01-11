625ec4d213cd2f4fb8c81a19b29753c82fcea479
chochem <40274384+chochem@users.noreply.github.com>
2024-01-11 16:45:04 +0000

    use GT for mod load checks

--- src/main/java/goodgenerator/crossmod/LoadedList.java
+++ src/main/java/goodgenerator/crossmod/LoadedList.java
@@ -1,12 +1,17 @@
 package goodgenerator.crossmod;
 
-import cpw.mods.fml.common.Loader;
+import static gregtech.api.enums.Mods.Automagy;
+import static gregtech.api.enums.Mods.GTPlusPlus;
+import static gregtech.api.enums.Mods.NewHorizonsCoreMod;
+import static gregtech.api.enums.Mods.ThaumicBases;
+import static gregtech.api.enums.Mods.ThaumicEnergistics;
+import static gregtech.api.enums.Mods.ThaumicTinkerer;
+import static gregtech.api.enums.Mods.WitchingGadgets;
 
 public class LoadedList {
 
     public static boolean GTPP;
     public static boolean GTNH_CORE;
-    public static boolean BOTDUSTRIES;
     public static boolean THAUMIC_BASES;
     public static boolean THAUMIC_TINKERER;
     public static boolean AUTOMAGY;
@@ -14,13 +19,12 @@ public class LoadedList {
     public static boolean THAUMIC_ENERGISTICS;
 
     public static void init() {
-        GTPP = Loader.isModLoaded("miscutils");
-        GTNH_CORE = Loader.isModLoaded("dreamcraft");
-        BOTDUSTRIES = Loader.isModLoaded("botdustries");
-        THAUMIC_BASES = Loader.isModLoaded("thaumicbases");
-        THAUMIC_TINKERER = Loader.isModLoaded("ThaumicTinkerer");
-        AUTOMAGY = Loader.isModLoaded("Automagy");
-        WITCHING_GADGETS = Loader.isModLoaded("WitchingGadgets");
-        THAUMIC_ENERGISTICS = Loader.isModLoaded("thaumicenergistics");
+        GTPP = GTPlusPlus.isModLoaded();
+        GTNH_CORE = NewHorizonsCoreMod.isModLoaded();
+        THAUMIC_BASES = ThaumicBases.isModLoaded();
+        THAUMIC_TINKERER = ThaumicTinkerer.isModLoaded();
+        AUTOMAGY = Automagy.isModLoaded();
+        WITCHING_GADGETS = WitchingGadgets.isModLoaded();
+        THAUMIC_ENERGISTICS = ThaumicEnergistics.isModLoaded();
     }
 }
